e7324e58583e876a794546d2820ae25f717eb95d
Godwin Olorunshola <gtwatt1001@gmail.com>
2019-10-26 16:44:37 +0100

    perfected login and get started view

---
+++ Jamii/LoginVC.swift
@@ -0,0 +1,9 @@
+//
+//  LoginVC.swift
+//  Jamii
+//
+//  Created by Godwin Olorunshola on 26/10/2019.
+//  Copyright © 2019 Godwin Olorunshola. All rights reserved.
+//
+
+import Foundation
---
+++ Jamii/custom views/RoundButton.swift
@@ -0,0 +1,32 @@
+//
+//  RoundButton.swift
+//  Jamii
+//
+//  Created by Godwin Olorunshola on 26/10/2019.
+//  Copyright © 2019 Godwin Olorunshola. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}

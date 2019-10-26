65b7647ddede297afed01990977df04180469575
Godwin Olorunshola <gtwatt1001@gmail.com>
2019-10-26 23:24:10 +0100

    Home design feeds cool

---
+++ Jamii/HomeTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  HomeTableViewCell.swift
+//  Jamii
+//
+//  Created by Godwin Olorunshola on 26/10/2019.
+//  Copyright © 2019 Godwin Olorunshola. All rights reserved.
+//
+
+import Foundation
